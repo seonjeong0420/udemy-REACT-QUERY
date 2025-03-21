774b7b1b869331ca8da3a56705654ae3a768b822
정선정(Seonjeong Jeong)/커머스개발팀/SKP <sunjeong0420@sk.com>
2025-03-21 10:17:13 +0900

    section3. useInfiniteQuery 사용방법

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -9,5 +10,37 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError, error } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading..</div>;
+  }
+
+  if (isError) {
+    return <div>Error ! {error.toString()}</div>;
+  }
+  return (
+    <>
+      {isFetching && <div className="loading">data fetching...</div>}
+      <InfiniteScroll
+        initialLoad={false} // InfiniteScroll에 의한 초기 페이지 로드 제거 (2페이지가 2번 로드되는 현상 방지)
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return <Person key={person.name} name={person.name} hairColor={person.hair_color} eyeColor={person.eye_color} />;
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,30 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, hasNextPage, fetchNextPage, isFetching, isLoading } = useInfiniteQuery({
+    queryKey: ["species-infinite"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) return <div className="loading">Loading..</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Data Fetching...</div>}
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) return fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((page) => {
+          return page.results.map((param, idx) => <Species key={`${param.name}${idx}`} name={param.name} language={param.language} averageLifespan={param.average_lifespan} />);
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
